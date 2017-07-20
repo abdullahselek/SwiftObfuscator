94a973b54a4afb0a071169bee1afd61eeb9db122
Abdullah Selek <abdullahselek@gmail.com>
2017-07-20 18:16:50 +0200

    create Obfuscator class

---
+++ SwiftObfuscator/SwiftObfuscator/Obfuscator.swift
@@ -0,0 +1,11 @@
+//
+//  Obfuscator.swift
+//  SwiftObfuscator
+//
+//  Created by Abdullah Selek on 20/07/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+class Obfuscator: NSObject {
+
+}
