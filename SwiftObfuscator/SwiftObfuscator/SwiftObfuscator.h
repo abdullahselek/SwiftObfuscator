e0887fc4f0a6421e84db39453ddf590746060e84
Abdullah Selek <abdullahselek@gmail.com>
2017-07-20 18:04:33 +0200

    create initial project

---
+++ SwiftObfuscator/SwiftObfuscator/SwiftObfuscator.h
@@ -0,0 +1,19 @@
+//
+//  SwiftObfuscator.h
+//  SwiftObfuscator
+//
+//  Created by Abdullah Selek on 20/07/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftObfuscator.
+FOUNDATION_EXPORT double SwiftObfuscatorVersionNumber;
+
+//! Project version string for SwiftObfuscator.
+FOUNDATION_EXPORT const unsigned char SwiftObfuscatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftObfuscator/PublicHeader.h>
+
+
