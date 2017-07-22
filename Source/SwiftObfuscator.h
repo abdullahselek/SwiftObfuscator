9c57c6b57791ac1cd262a474d12c47590e3d2e0c
Abdullah Selek <abdullahselek@gmail.com>
2017-07-22 17:47:09 +0100

    create umbrella header for frameworks

---
+++ Source/SwiftObfuscator.h
@@ -0,0 +1,19 @@
+//
+//  SwiftObfuscator-iOS.h
+//  SwiftObfuscator-iOS
+//
+//  Created by Abdullah Selek on 22/07/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftObfuscator.
+FOUNDATION_EXPORT double SwiftObfuscator_VersionNumber;
+
+//! Project version string for SwiftObfuscator.
+FOUNDATION_EXPORT const unsigned char SwiftObfuscator_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftObfuscator/PublicHeader.h>
+
+
